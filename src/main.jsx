5d8c019e3b16db9e9979ae54d7ddcec8475ecfbd
Romano Ycoy <romanoycoy1@gmail.com>
2025-10-28 22:18:14 +0800

    chore: encapsulate app with authProvider so the system can know that the user is logged in

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,20 @@ import { ThemeProvider } from './app/providers/ThemeProvider'
 import { AppRouter } from './app/router/AppRouter'
 import { Toaster } from "@/shared/ui/sonner"
 import { StudentProvider } from './entities/student/model/StudentProvider'
+import { AuthProvider } from './features/auth/context/AuthContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <StudentProvider>
-          <AppRouter />
-          <Toaster />
-        </StudentProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <StudentProvider>
+            <AppRouter />
+            <Toaster />
+          </StudentProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 )
